4bafe954164bdbbbc918bb5c15f6f3a0be1cb187
annibuliful <lagmanmc@gmail.com>
2024-01-18 12:40:26 +0700

    feat: frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/charts/styles.css';
+import 'dayjs/locale/th';
+import { MantineProvider } from '@mantine/core';
+import { DatesProvider } from '@mantine/dates';
+import { Provider as JotaiProvider } from 'jotai';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <JotaiProvider>
+      <MantineProvider>
+        <DatesProvider settings={{ locale: 'th' }}>
+          <App />
+        </DatesProvider>
+      </MantineProvider>
+    </JotaiProvider>
+  </React.StrictMode>
+);
